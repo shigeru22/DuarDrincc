ffc9dc42433f5405bd4794bca6fe6204d1313384
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-04-13 22:14:32 +0700

    Add flavor slideshow in InsertMoney

---
+++ Assets/Scripts/FlavorSlideShow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlavorSlideShow : MonoBehaviour
+{
+    public GameObject[] flavor;
+    int flavorCount;
+    int counter;
+    float time;
+    bool shiftTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flavorCount = flavor.Length;
+        counter = 0;
+        time = 0;
+        shiftTrigger = false;
+
+        // first run, set first flavor to active and others to inactive
+        flavor[counter].SetActive(true);
+        for (int i = 1; i < flavorCount; i++) flavor[i].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= 1f)
+        {
+            shiftTrigger = true;
+            time -= 1;
+        }
+
+        if(shiftTrigger == true)
+        {
+            flavor[counter++].SetActive(false); // disable current (counter) object and increment counter
+
+            if (counter == flavorCount) counter = 0; // if current counter equals flavorCount size, change counter value to zero
+            flavor[counter].SetActive(true); // enable current (counter) object
+
+            shiftTrigger = false; // change shiftTrigger value to false
+        }
+    }
+}
