2ad47b8843ca34cec7e6b5b0794d84595b1ebcfe
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-04-20 22:33:18 +0700

    Add MenuSelection to store selection and FlavorSelect scripts

---
+++ Assets/Scripts/FlavorSelect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlavorSelect : MonoBehaviour
+{
+    public Button[] flavors; // set size to 8 and follow MenuSelection.cs for flavors' order
+    public GameObject thisPage; // set to MenuSelect
+    public GameObject nextPage; // set to TempSelect
+
+    MenuSelection store;
+
+    void Start()
+    {
+        // find gameobject with tag "SelectedStore", which is Menus, and access this component
+        store = GameObject.FindGameObjectWithTag("SelectedStore").GetComponent<MenuSelection>();
+
+        // in this loop, listener must be added in its own scope
+        // else its parameters will follow the native looping i instead
+        for(int i = 0; i < flavors.Length; i++)
+        {
+            int temp = i;
+            flavors[i].onClick.AddListener(delegate
+            {
+                FlavorClick(temp);
+            });
+        }
+    }
+
+    void FlavorClick(int idx)
+    {
+        store.selectedFlavor = idx;
+        thisPage.SetActive(false); // hide this page
+        nextPage.SetActive(true); // and show next page
+    }
+}
---
+++ Assets/Scripts/MenuSelection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSelection : MonoBehaviour
+{
+    /*
+     * Note:
+     * In this project, flavor elements must be in this order:
+     * Element 1: Chocolate
+     *         2: Vanilla
+     *         3: Strawberry
+     *         4: Blueberry
+     *         5: GreenTea
+     *         6: Mint
+     *         7: Lemon
+     *         8: Coffee
+     */
+
+    public int insertedMoney;
+    public int selectedFlavor;
+    public int selectedTemp;
+    public int selectedTopping;
+}
