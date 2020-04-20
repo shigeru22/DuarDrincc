5ddd642bf2a3f5f6bfff8dc990a631335c582cd3
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-04-20 22:41:58 +0700

    Add TempSelect scripts

---
+++ Assets/Scripts/TempSelect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TempSelect : MonoBehaviour
+{
+    public Button[] temps; // set size to 2 and add 0 for hot and 1 to cold
+    public GameObject thisPage; // set to TempSelect
+    public GameObject nextPage; // set to ToppingSelect
+
+    MenuSelection store;
+
+    void Start()
+    {
+        // find gameobject with tag "SelectedStore", which is Menus, and access this component
+        store = GameObject.FindGameObjectWithTag("SelectedStore").GetComponent<MenuSelection>();
+
+        // same as FlavorSelect.cs, add variable in its own scope
+        for (int i = 0; i < temps.Length; i++)
+        {
+            int temp = i;
+            temps[i].onClick.AddListener(delegate
+            {
+                TempClick(temp);
+            });
+        }
+    }
+
+    void TempClick(int idx)
+    {
+        store.selectedTemp = idx;
+        thisPage.SetActive(false); // hide this page
+        nextPage.SetActive(true); // and show next page
+    }
+}
