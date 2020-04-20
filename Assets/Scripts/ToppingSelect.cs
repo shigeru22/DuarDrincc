75f1bd97f5514934a0a8265257ed5cbfa5f1d1e2
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-04-20 22:50:18 +0700

    Add ToppingSelect scripts

---
+++ Assets/Scripts/ToppingSelect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToppingSelect : MonoBehaviour
+{
+    public Button[] toppings; // set size to 7 and follow MenuSelection.cs for toppings' order
+    public GameObject thisPage; // set to ToppingSelect
+    public GameObject nextPage; // set to Confirmation
+
+    MenuSelection store;
+
+    void Start()
+    {
+        // find gameobject with tag "SelectedStore", which is Menus, and access this component
+        store = GameObject.FindGameObjectWithTag("SelectedStore").GetComponent<MenuSelection>();
+
+        // same as FlavorSelect.cs, add variable in its own scope
+        for (int i = 0; i < toppings.Length; i++)
+        {
+            int temp = i;
+            toppings[i].onClick.AddListener(delegate
+            {
+                ToppingClick(temp);
+            });
+        }
+    }
+
+    void ToppingClick(int idx)
+    {
+        store.selectedTopping = idx;
+        thisPage.SetActive(false); // hide this page
+        nextPage.SetActive(true); // and show next page
+    }
+}
