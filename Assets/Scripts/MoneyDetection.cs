0ca936be55a5dcfb0b80c4fd107bbfbbf8f8ba09
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-04-21 21:32:27 +0700

    Switch active component if money inserted

---
+++ Assets/Scripts/MoneyDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyDetection : MonoBehaviour
+{
+    public GameObject thisPage;
+    public GameObject nextPage;
+
+    MenuSelection store;
+
+    void Start()
+    {
+        // find gameobject with tag "SelectedStore", which is Menus, and access this component
+        store = GameObject.FindGameObjectWithTag("SelectedStore").GetComponent<MenuSelection>();
+    }
+
+    void Update()
+    {
+        // show next page if money has been inserted (higher than 0)
+        if(store.insertedMoney > 0)
+        {
+            thisPage.SetActive(false);
+            nextPage.SetActive(true);
+        }
+    }
+}
