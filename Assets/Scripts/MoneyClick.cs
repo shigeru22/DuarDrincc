ee825084e62ecb7a2e1e254d7b5e2ad707968c57
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-04-28 14:12:17 +0700

    Add money window animation and inserted money addition

---
+++ Assets/Scripts/MoneyClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyClick : MonoBehaviour
+{
+    public Button seribu;
+    public Button duaRibu;
+    public Button limaRibu;
+    public Button sepuluhRibu;
+
+    MenuSelection store;
+
+    void Start()
+    {
+        store = GameObject.FindGameObjectWithTag("SelectedStore").GetComponent<MenuSelection>();
+
+        // money listener
+        seribu.onClick.AddListener(delegate { addMoney(1000); });
+        duaRibu.onClick.AddListener(delegate { addMoney(2000); });
+        limaRibu.onClick.AddListener(delegate { addMoney(5000); });
+        sepuluhRibu.onClick.AddListener(delegate { addMoney(10000); });
+    }
+
+    void addMoney(int amount)
+    {
+        store.insertedMoney += amount;
+    }
+}
