fef78f9ddb91c0e0ae766f3e8a3aaadcb1705595
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-04-28 13:15:50 +0700

    Add window for money and animation

---
+++ Assets/Scripts/WindowClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowClick : MonoBehaviour
+{
+    public Animator windowAnimator;
+    public Button slot;
+
+    void Start()
+    {
+        slot.onClick.AddListener(delegate
+        {
+            windowAnimator.SetTrigger("SlotClicked");
+        });
+    }
+}
