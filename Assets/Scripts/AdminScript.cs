92621fb9ea99be61011b9a27455e072e0f57ee73
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-05-03 15:30:43 +0700

    Implement Konami code
    This time, entering Up, Up, Down, Down, Left, Right, Left, Right, B, A, Enter (in order) will output "Hello, world!" to Unity console.

---
+++ Assets/Scripts/AdminScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdminScript : MonoBehaviour
+{
+    private string[] konamiCode = { "UpArrow", "UpArrow", "DownArrow", "DownArrow", "LeftArrow", "RightArrow", "LeftArrow", "RightArrow", "B", "A", "Return" };
+    private int currPos = 0;
+ 
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey && Input.anyKeyDown && e.keyCode.ToString() != "None") Insert(e.keyCode);
+    }
+
+    void Insert(KeyCode s)
+    {
+        if (s.ToString() == konamiCode[currPos])
+        {
+            currPos++;
+            if ((currPos + 1) > konamiCode.Length)
+            {
+                RunEasterEgg();
+                currPos = 0;
+            }
+        }
+        else
+        {
+            if (currPos != 0) Debug.Log("Konami code failed in " + (currPos + 1));
+            currPos = 0;
+        }
+    }
+
+    void RunEasterEgg()
+    {
+        Debug.Log("Hello, world!");
+    }
+}
