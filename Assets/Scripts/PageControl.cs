d24a93898d07ebb754d19e0f32af6e117c876d9c
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-05-03 14:44:31 +0700

    Detect sold out flavors when shown

--- Assets/Scripts/PageControl.cs
+++ Assets/Scripts/PageControl.cs
@@ -5,13 +5,25 @@ using UnityEngine.UI;
 
 public class PageControl : MonoBehaviour
 {
+    public Button[] flavors;
     public GameObject[] pages;
+    public GameObject[] soldOuts;
+
     public GameObject back;
     public GameObject next;
     int currPage;
 
+    MenuSelection store;
+    DrinccStorage storage;
+
+    [System.NonSerialized]
+    public bool runOnce = false; // so update part won't keep looping
+
     void Start()
     {
+        store = GameObject.FindGameObjectWithTag("SelectedStore").GetComponent<MenuSelection>();
+        storage = GameObject.FindGameObjectWithTag("DrinccStorage").GetComponent<DrinccStorage>();
+
         // show first page and hide other pages
         currPage = 0;
         pages[currPage].SetActive(true);
@@ -26,6 +38,20 @@ public class PageControl : MonoBehaviour
         next.GetComponent<Button>().onClick.AddListener(NextClick);
     }
 
+    void Update()
+    {
+        if(store.insertedMoney > 0 && runOnce == false)
+        {
+            SoldOutUpdate();
+            runOnce = true;
+        }
+        if(store.insertedMoney == 0 && runOnce == true)
+        {
+            SoldOutUpdate();
+            runOnce = false;
+        }
+    }
+
     void BackClick()
     {
         if (currPage != 0)
@@ -36,6 +62,8 @@ public class PageControl : MonoBehaviour
             // show next button and if first page hide back button
             next.SetActive(true);
             if (currPage == 0) back.SetActive(false);
+
+            SoldOutUpdate();
         }
         else Debug.Log("This is the first page!");
     }
@@ -50,7 +78,26 @@ public class PageControl : MonoBehaviour
             // show back button and if last page hide next button
             back.SetActive(true);
             if (currPage == pages.Length - 1) next.SetActive(false);
+
+            SoldOutUpdate();
         }
         else Debug.Log("This is the last page!");
+    }
+
+    void SoldOutUpdate()
+    {
+        for (int i = 0; i < soldOuts.Length; i++)
+        {
+            if (storage.flavors[(((currPage + 1) * 4) - 1) - i] == 0)
+            {
+                flavors[(((currPage + 1) * 4) - 1) - i].interactable = false;
+                soldOuts[soldOuts.Length - 1 - (i % soldOuts.Length)].SetActive(true);
+            }
+            else
+            {
+                flavors[(((currPage + 1) * 4) - 1) - i].interactable = true;
+                soldOuts[soldOuts.Length - 1 - (i % soldOuts.Length)].SetActive(false);
+            }
+        }
     }
 }
