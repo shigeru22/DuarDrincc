db2b11750c931c670c41d97cdaaa5bfaf0290733
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-05-05 15:30:53 +0700

    Automatically close MoneyWindow if money inserted is 30000 or more

--- Assets/Scripts/WindowClick.cs
+++ Assets/Scripts/WindowClick.cs
@@ -8,11 +8,34 @@ public class WindowClick : MonoBehaviour
     public Animator windowAnimator;
     public Button slot;
 
+    MenuSelection store;
+
+    bool open;
+
     void Start()
     {
-        slot.onClick.AddListener(delegate
+        store = GameObject.FindGameObjectWithTag("SelectedStore").GetComponent<MenuSelection>();
+
+        slot.onClick.AddListener(Trigger);
+    }
+
+    void Update()
+    {
+        if(store.insertedMoney >= 30000 && open == true)
         {
             windowAnimator.SetTrigger("SlotClicked");
-        });
+            open = false;
+        }
+    }
+
+    void Trigger()
+    {
+        if (store.insertedMoney < 30000)
+        {
+            windowAnimator.SetTrigger("SlotClicked");
+
+            if (open == true) open = false;
+            else open = true;
+        }
     }
 }
