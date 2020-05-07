7807530fdc5b300aa25814e087a37898e49440ec
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-05-07 21:17:01 +0700

    Fix change animation only run once

--- Assets/Scripts/DrinccAnimation.cs
+++ Assets/Scripts/DrinccAnimation.cs
@@ -167,6 +167,7 @@ public class DrinccAnimation : MonoBehaviour
                 {
                     processing.SetActive(false);
                     takeChange.SetActive(true);
+                    change.SetActive(true);
 
                     // change animation
                     if (store.insertedMoney < 10000) change5k.SetTrigger("Run");
